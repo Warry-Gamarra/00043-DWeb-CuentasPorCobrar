eb64c2390db55c1196bfdc7f9429190b7e6634bb
wgamarra <wgamarra2310@gmail.com>
2021-12-01 13:28:51 -0500

    Columnas para migracion de pagos

--- src/database/00043-CuentasPorCobrar/BD_OCEF_CtasPorCobrar/5. BD_OCEF_CtasPorCobrar - Migracion de Temporal - De pagos.sql
+++ src/database/00043-CuentasPorCobrar/BD_OCEF_CtasPorCobrar/5. BD_OCEF_CtasPorCobrar - Migracion de Temporal - De pagos.sql
@@ -1,119 +1,50 @@
 USE BD_OCEF_CtasPorCobrar
 GO
 
-
 IF EXISTS (SELECT * FROM INFORMATION_SCHEMA.ROUTINES WHERE ROUTINE_TYPE = 'PROCEDURE' AND ROUTINE_NAME = 'USP_IU_GrabarProcesosYCuentasMigracion')
 	DROP PROCEDURE [dbo].[USP_IU_GrabarProcesosYCuentasMigracion]
 GO
 
-CREATE PROCEDURE USP_IU_GrabarProcesosYCuentasMigracion
-	@I_AnioIni	  int = NULL,
-	@I_AnioFin	  int = NULL,
-	@B_Resultado  bit output,
-	@T_Message	  nvarchar(4000) OUTPUT	
-AS
---declare @B_Resultado  bit, @I_AnioIni int, @I_AnioFin int,
---		@T_Message	  nvarchar(4000)
---exec USP_IU_GrabarProcesosYCuentasMigracion @B_Resultado = @B_Resultado output, @T_Message = @T_Message output
---select @B_Resultado as resultado, @T_Message as mensaje
+
+
+IF NOT EXISTS (SELECT * FROM INFORMATION_SCHEMA.COLUMNS WHERE COLUMN_NAME = 'B_Migrado' AND TABLE_NAME = 'TR_ObligacionAluCab')
 BEGIN
-	DECLARE @D_FecProceso datetime = GETDATE() 
-	DECLARE @Tbl_outputProceso AS TABLE (T_Action varchar(20), I_RowID int)
-	DECLARE @Tbl_outputCtas AS TABLE (T_Action varchar(20), I_RowID int)
-	DECLARE @Tbl_outputCtasCat AS TABLE (T_Action varchar(20), I_RowID int)
-	DECLARE @I_Proc_Inserted int = 0
-	DECLARE @I_Proc_Updated int = 0
-	DECLARE @I_Ctas_Inserted int = 0
-	DECLARE @I_Ctas_Updated int = 0
-	DECLARE @I_CtaCat_Inserted int = 0
-	DECLARE @I_CtaCat_Updated int = 0
+ALTER TABLE TR_ObligacionAluCab
+	ADD B_Migrado    bit  NOT NULL DEFAULT(0)
 
-	BEGIN TRANSACTION;
-	BEGIN TRY 
-		SET @I_AnioIni = (SELECT ISNULL(@I_AnioIni, 0))
-		SET @I_AnioFin = (SELECT ISNULL(@I_AnioFin, 3000))
-
-		SET IDENTITY_INSERT TC_Proceso ON;
-
-		MERGE INTO TC_Proceso AS TRG
-		USING (SELECT * FROM BD_OCEF_MigracionTP.dbo.TR_MG_CpDes WHERE B_Migrable = 1 AND I_Anio BETWEEN @I_AnioIni AND @I_AnioFin) AS SRC
-		ON TRG.I_ProcesoID = SRC.CUOTA_PAGO 
-		WHEN NOT MATCHED BY TARGET THEN 
-			 INSERT (I_ProcesoID, I_CatPagoID, T_ProcesoDesc, I_Anio, I_Periodo, N_CodBanco, D_FecVencto, I_Prioridad, B_Mora, B_Migrado, D_FecCre, B_Habilitado, B_Eliminado)
-			 VALUES (CUOTA_PAGO, I_CatPagoID, DESCRIPCIO, I_Anio, I_Periodo, CODIGO_BNC, FCH_VENC, PRIORIDAD, 
-					CASE C_MORA WHEN 'VERDADERO' THEN 1 WHEN 'FALSO' THEN 0 WHEN 'True' THEN 1 WHEN 'False' THEN 0 ELSE NULL END, 1, @D_FecProceso, 1, ELIMINADO)
-		WHEN MATCHED AND TRG.B_Migrado = 1 AND TRG.I_UsuarioMod IS NULL THEN 
-			 UPDATE SET I_CatPagoID = SRC.I_CatPagoID, 
-					 T_ProcesoDesc = SRC.DESCRIPCIO, 
-					 I_Anio = SRC.I_Anio, 
-					 I_Periodo = SRC.I_Periodo,
-					 N_CodBanco = SRC.CODIGO_BNC, 
-					 D_FecVencto = SRC.FCH_VENC, 
-					 I_Prioridad = SRC.PRIORIDAD, 
-					 D_FecMod = @D_FecProceso,
-					 B_Mora = (CASE SRC.C_MORA WHEN 'VERDADERO' THEN 1 WHEN 'FALSO' THEN 0 WHEN 'True' THEN 1 WHEN 'False' THEN 0 ELSE NULL END)
-		OUTPUT $action, SRC.I_RowID INTO @Tbl_outputProceso;
-		
-		SET IDENTITY_INSERT TC_Proceso OFF
-		
-		MERGE INTO TI_CtaDepo_Proceso AS TRG
-		USING (SELECT CD.I_CtaDepositoID, TP_CD.* FROM BD_OCEF_MigracionTP.dbo.TR_MG_CpDes TP_CD
-				INNER JOIN TC_CuentaDeposito CD ON CD.C_NumeroCuenta COLLATE DATABASE_DEFAULT = TP_CD.N_CTA_CTE COLLATE DATABASE_DEFAULT
-				WHERE B_Migrable = 1 AND I_Anio BETWEEN @I_AnioIni AND @I_AnioFin) AS SRC
-		ON TRG.I_ProcesoID = SRC.CUOTA_PAGO AND TRG.I_CtaDepositoID = SRC.I_CtaDepositoID
-		WHEN NOT MATCHED BY TARGET THEN
-			INSERT (I_CtaDepositoID, I_ProcesoID, B_Habilitado, B_Eliminado, D_FecCre)
-			VALUES (I_CtaDepositoID, CUOTA_PAGO, 1, ELIMINADO, @D_FecProceso)
-		WHEN MATCHED AND TRG.I_UsuarioCre IS NULL AND TRG.I_UsuarioMod IS NULL THEN
-			UPDATE SET	B_Eliminado = ELIMINADO,
-						D_FecMod = @D_FecProceso
-		OUTPUT $action, SRC.I_RowID INTO @Tbl_outputCtas;
-
-
-		MERGE INTO TC_CuentaDeposito_CategoriaPago AS TRG
-		USING (SELECT DISTINCT CD.I_CtaDepositoID, TP_CD.I_CatPagoID FROM BD_OCEF_MigracionTP.dbo.TR_MG_CpDes TP_CD
-				INNER JOIN TC_CuentaDeposito CD ON CD.C_NumeroCuenta COLLATE DATABASE_DEFAULT = TP_CD.N_CTA_CTE COLLATE DATABASE_DEFAULT
-				WHERE B_Migrable = 1 AND I_Anio BETWEEN @I_AnioIni AND @I_AnioFin) AS SRC
-		ON TRG.I_CatPagoID = SRC.I_CatPagoID AND TRG.I_CtaDepositoID = SRC.I_CtaDepositoID
-		WHEN NOT MATCHED BY TARGET THEN
-			INSERT (I_CtaDepositoID, I_CatPagoID, B_Habilitado, B_Eliminado, D_FecCre)
-			VALUES (I_CtaDepositoID, I_CatPagoID, 1, 0, @D_FecProceso)
-		WHEN MATCHED AND TRG.I_UsuarioCre IS NULL AND TRG.I_UsuarioMod IS NULL THEN
-			UPDATE SET	D_FecMod = @D_FecProceso
-		OUTPUT $action, SRC.I_CatPagoID INTO @Tbl_outputCtasCat;
-		
-
-		UPDATE	BD_OCEF_MigracionTP.dbo.TR_MG_CpDes 
-		SET		B_Migrado = 1, 
-				D_FecMigrado = @D_FecProceso
-		WHERE	I_RowID IN (SELECT I_RowID FROM @Tbl_outputProceso)
-
-		UPDATE	BD_OCEF_MigracionTP.dbo.TR_MG_CpDes 
-		SET		T_Observacion = '000 - EXTERNO: ('+ CONVERT(varchar, @D_FecProceso, 112) + '). La cuota de pago ha sido ingresada o modificada desde una fuente externa.|',
-				B_Migrado = 0 
-		WHERE	I_RowID IN (SELECT CD.I_RowID FROM BD_OCEF_MigracionTP.dbo.TR_MG_CpDes CD LEFT JOIN @Tbl_outputProceso O ON CD.I_RowID = o.I_RowID 
-							WHERE O.I_RowID IS NULL)
-
-		SET @I_Proc_Inserted = (SELECT COUNT(*) FROM @Tbl_outputProceso WHERE T_Action = 'INSERT')
-		SET @I_Proc_Updated = (SELECT COUNT(*) FROM @Tbl_outputProceso WHERE T_Action = 'UPDATE')
-		SET @I_Ctas_Inserted = (SELECT COUNT(*) FROM @Tbl_outputCtas WHERE T_Action = 'INSERT')
-		SET @I_Ctas_Updated = (SELECT COUNT(*) FROM @Tbl_outputCtas WHERE T_Action = 'UPDATE')
-		SET @I_CtaCat_Inserted = (SELECT COUNT(*) FROM @Tbl_outputCtasCat WHERE T_Action = 'INSERT')
-		SET @I_CtaCat_Updated = (SELECT COUNT(*) FROM @Tbl_outputCtasCat WHERE T_Action = 'UPDATE')
-
-		SELECT @I_Proc_Inserted AS proc_count_insert, @I_Proc_Updated AS proc_count_update, 
-				@I_Ctas_Inserted AS ctas_count_insert, @I_Ctas_Updated AS ctas_count_update,
-				@I_CtaCat_Inserted AS ctas_cat_count_insert, @I_CtaCat_Updated AS ctas_cat_count_update
-
-		COMMIT TRANSACTION;
-
-		SET @B_Resultado = 1
-		SET @T_Message = 'Ok'
-	END TRY
-	BEGIN CATCH
-		SET @B_Resultado = 0
-		SET @T_Message = ERROR_MESSAGE() + ' LINE: ' + CAST(ERROR_LINE() AS varchar(10)) 
-		ROLLBACK TRANSACTION;
-	END CATCH
+	UPDATE TR_ObligacionAluCab SET B_Migrado = 0
 END
 GO
+
+
+IF NOT EXISTS (SELECT * FROM INFORMATION_SCHEMA.COLUMNS WHERE COLUMN_NAME = 'B_Migrado' AND TABLE_NAME = 'TR_ObligacionAluDet')
+BEGIN
+	ALTER TABLE TR_ObligacionAluDet
+		ADD B_Migrado    bit  NOT NULL DEFAULT(0)
+
+	UPDATE TR_ObligacionAluDet SET B_Migrado = 0
+END
+GO
+
+
+IF NOT EXISTS (SELECT * FROM INFORMATION_SCHEMA.COLUMNS WHERE COLUMN_NAME = 'B_Migrado' AND TABLE_NAME = 'TRI_PagoProcesadoUnfv')
+BEGIN
+	ALTER TABLE TRI_PagoProcesadoUnfv
+		ADD B_Migrado    bit  NOT NULL DEFAULT(0)
+
+	UPDATE TRI_PagoProcesadoUnfv SET B_Migrado = 0
+END
+GO
+
+
+IF NOT EXISTS (SELECT * FROM INFORMATION_SCHEMA.COLUMNS WHERE COLUMN_NAME = 'B_Migrado' AND TABLE_NAME = 'TR_PagoBanco')
+BEGIN
+	ALTER TABLE TR_PagoBanco
+		ADD B_Migrado    bit  NOT NULL DEFAULT(0)
+
+	UPDATE TR_PagoBanco SET B_Migrado = 0
+END
+GO
+
+
+
