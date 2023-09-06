eb326bbda5e4341cfb89995863371d331007c08f
Humberto Manyari <manyari_571@hotmail.com>
2023-09-05 19:03:12 -0500

    Avance registro de tasas

--- src/database/00043-CuentasPorCobrar/Scripts Produccón/XX. BD_OCEF_CtasPorCobrar - mejoras.sql
+++ src/database/00043-CuentasPorCobrar/Scripts Produccón/XX. BD_OCEF_CtasPorCobrar - mejoras.sql
@@ -27,7 +27,7 @@ GO
 SET IDENTITY_INSERT dbo.TC_Servicios OFF
 GO
 DBCC CHECKIDENT ('dbo.TC_Servicios', RESEED, 26);  
-GO  
+GO
 
 INSERT dbo.TI_CtaDepo_Servicio(I_CtaDepositoID, I_ServicioID, B_Habilitado, B_Eliminado, I_UsuarioCre, D_FecCre) VALUES(2, 5, 1, 0, 1, GETDATE())
 INSERT dbo.TI_CtaDepo_Servicio(I_CtaDepositoID, I_ServicioID, B_Habilitado, B_Eliminado, I_UsuarioCre, D_FecCre) VALUES(7, 6, 1, 0, 1, GETDATE())
@@ -39,6 +39,14 @@ INSERT dbo.TI_CtaDepo_Servicio(I_CtaDepositoID, I_ServicioID, B_Habilitado, B_El
 INSERT dbo.TI_CtaDepo_Servicio(I_CtaDepositoID, I_ServicioID, B_Habilitado, B_Eliminado, I_UsuarioCre, D_FecCre) VALUES(4, 25, 1, 0, 1, GETDATE())
 INSERT dbo.TI_CtaDepo_Servicio(I_CtaDepositoID, I_ServicioID, B_Habilitado, B_Eliminado, I_UsuarioCre, D_FecCre) VALUES(1, 21, 1, 0, 1, GETDATE())
 INSERT dbo.TI_CtaDepo_Servicio(I_CtaDepositoID, I_ServicioID, B_Habilitado, B_Eliminado, I_UsuarioCre, D_FecCre) VALUES(4, 24, 1, 0, 1, GETDATE())
+INSERT dbo.TI_CtaDepo_Servicio(I_CtaDepositoID, I_ServicioID, B_Habilitado, B_Eliminado, I_UsuarioCre, D_FecCre) VALUES(7, 1, 1, 0, 1, GETDATE())
+INSERT dbo.TI_CtaDepo_Servicio(I_CtaDepositoID, I_ServicioID, B_Habilitado, B_Eliminado, I_UsuarioCre, D_FecCre) VALUES(7, 2, 1, 0, 1, GETDATE())
+INSERT dbo.TI_CtaDepo_Servicio(I_CtaDepositoID, I_ServicioID, B_Habilitado, B_Eliminado, I_UsuarioCre, D_FecCre) VALUES(2, 12, 1, 0, 1, GETDATE())
+INSERT dbo.TI_CtaDepo_Servicio(I_CtaDepositoID, I_ServicioID, B_Habilitado, B_Eliminado, I_UsuarioCre, D_FecCre) VALUES(2, 13, 1, 0, 1, GETDATE())
+INSERT dbo.TI_CtaDepo_Servicio(I_CtaDepositoID, I_ServicioID, B_Habilitado, B_Eliminado, I_UsuarioCre, D_FecCre) VALUES(2, 15, 1, 0, 1, GETDATE())
+INSERT dbo.TI_CtaDepo_Servicio(I_CtaDepositoID, I_ServicioID, B_Habilitado, B_Eliminado, I_UsuarioCre, D_FecCre) VALUES(4, 16, 1, 0, 1, GETDATE())
+INSERT dbo.TI_CtaDepo_Servicio(I_CtaDepositoID, I_ServicioID, B_Habilitado, B_Eliminado, I_UsuarioCre, D_FecCre) VALUES(4, 26, 1, 0, 1, GETDATE())
+INSERT dbo.TI_CtaDepo_Servicio(I_CtaDepositoID, I_ServicioID, B_Habilitado, B_Eliminado, I_UsuarioCre, D_FecCre) VALUES(2, 19, 1, 0, 1, GETDATE())
 GO
 
 
